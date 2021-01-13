fb8a9611ad1949376514e24c33584cd062f4e617
Hunter Posey <ihposey@crimson.ua.edu>
2021-01-13 10:58:01 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value
+  // const taskItem = document.createElement("li")
+  // taskItem.innerTEXT = newTask
+  // taskList.appendChild(taskItem)
+  taskList.innerHTML += `
+      <li>${newTask}
+        <button data-action="delete">delete</button>
+      </li>
+    `
+  taskForm.reset()
+})
+
+taskList.addEventListener('click', function(e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove()
+  }
+})
